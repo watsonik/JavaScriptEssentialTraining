65022e894fe079b63515c608fae1306163a4363d
Ihar Sakun <watsonik@gmail.com>
2019-10-07 00:47:06 +0300

    Functions

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-var date = new Date();
-document.body.innerHTML = "<h1> Today is " + date + "</h1>";
+function findBiggestFraction(a,b) {
+    var result;
+    a>b ? result = ["firstFraction", a] : result = ["secondFraction", b];
+    return result;
+}
+
+var firstFraction = 3/4;
+var secondFraction = 5/7;
+
+var fractionResult = findBiggestFraction(firstFraction,secondFraction);
+
+console.log("First fraction result: ", firstFraction);
+console.log("Second fraction result: ", secondFraction);
+console.log("Fraction " + fractionResult[0] + " with a value of " + fractionResult[1] + " is the biggest!");
