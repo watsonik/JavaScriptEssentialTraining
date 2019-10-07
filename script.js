e789639c6f220e7231cfc0423a3d135f2a2c4da8
Ihar Sakun <watsonik@gmail.com>
2019-10-07 23:39:57 +0300

    Constant

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-var firstFraction = 3/4;
-var secondFraction = 5/7;
-
-var theBiggest = (function (a, b) {
-    var result;
-    a > b ? result = ["a", a] : result = ["b", b];
-    return result;
-})(firstFraction, secondFraction);
-
-console.log(theBiggest);
+const MYCONSTANT = 5;
+console.log(MYCONSTANT);
+//MYCONSTANT=6;
