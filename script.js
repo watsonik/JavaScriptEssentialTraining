c1a23f99391a57dfba801acfdb595f6c83d38f85
Ihar Sakun <watsonik@gmail.com>
2019-10-08 02:10:49 +0300

    Objects

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-function logScope() {
-    let localVar=2;
-    if (localVar){
-        let localVar = "I'm differernt!";
-        console.log("nested localVar", localVar);
-    }
-    console.log("logScope localVar: ", localVar);
-}
+// var course = new Object();
+// course.title = "JavaScript Essential Training";
+// course.instructor = "Ihar Sakun";
+// course.level = 1;
+// course.published = true;
+// course.views = 0;
 
-logScope();
+var course = {
+    title: "JavaScript Essential Training",
+    instructor: "Ihar Sakun",
+    level: 1,
+    published: true,
+    views: 0,
+    updateViews: function () {
+        return ++course.views
+    }
+};
+console.log(course.views);
+course.updateViews();
+console.log(course.views);
+
+
+
+
+
