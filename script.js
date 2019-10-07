f1b2f5663b3616e647820ee04728fe49418a3145
Ihar Sakun <watsonik@gmail.com>
2019-10-08 01:39:23 +0300

    Let vs var

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const MYCONSTANT = 5;
-console.log(MYCONSTANT);
-//MYCONSTANT=6;
+function logScope() {
+    let localVar=2;
+    if (localVar){
+        let localVar = "I'm differernt!";
+        console.log("nested localVar", localVar);
+    }
+    console.log("logScope localVar: ", localVar);
+}
+
+logScope();
