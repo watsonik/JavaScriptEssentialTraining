66e04b087794209bbc6cfaf539b410d88439b0be
Ihar Sakun <watsonik@gmail.com>
2019-10-08 03:01:13 +0300

    Object constructors

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-// var course = new Object();
-// course.title = "JavaScript Essential Training";
-// course.instructor = "Ihar Sakun";
-// course.level = 1;
-// course.published = true;
-// course.views = 0;
+function Course(title, instructor, level, published, views) {
+    this.title = title;
+    this.instructor = instructor;
+    this.level = level;
+    this.published = published;
+    this.views = views;
+    this.updateViews = function () {
+        return ++this.views;
+    };
+}
 
-var course = {
-    title: "JavaScript Essential Training",
-    instructor: "Ihar Sakun",
-    level: 1,
-    published: true,
-    views: 0,
-    updateViews: function () {
-        return ++course.views
-    }
-};
-console.log(course.views);
-course.updateViews();
-console.log(course.views);
+var courses=[
+    new Course("JavaScript Essential Training","Ihar Sakun",1,true,0),
+    new Course("Java Training","Watson",1,true,123)
+];
 
+// var course01 = new Course("JavaScript Essential Training","Ihar Sakun",1,true,0);
+
+console.log(courses[1].updateViews());
 
 
 
