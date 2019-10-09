af80f995ba231b5021258981fd386775eede0767
Ihar Sakun <watsonik@gmail.com>
2019-10-09 10:54:49 +0300

    Closures

--- script.js
+++ script.js
@@ -1,23 +1,12 @@
-function Course(title, instructor, level, published, views) {
-    this.title = title;
-    this.instructor = instructor;
-    this.level = level;
-    this.published = published;
-    this.views = views;
-    this.updateViews = function () {
-        return ++this.views;
-    };
+function doSomeMath(){
+    var a=5;
+    var b=4;
+    var sum=a+b;
+    function multiply() {
+        var result = a*b;
+        return result;
+    }
+    return multiply;
 }
-
-var courses=[
-    new Course("JavaScript Essential Training","Ihar Sakun",1,true,0),
-    new Course("Java Training","Watson",1,true,123)
-];
-
-// var course01 = new Course("JavaScript Essential Training","Ihar Sakun",1,true,0);
-
-console.log(courses[1].updateViews());
-
-
-
-
+var theResult = doSomeMath();
+console.log("The result: ", theResult());
