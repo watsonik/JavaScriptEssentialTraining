8de92ab2025d6a5777720ee709a00bfab37b93e2
Ihar Sakun <watsonik@gmail.com>
2019-10-10 17:20:43 +0300

    Add DOM elements

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,11 @@
-const CTAELEMENT = document.querySelector(".cta a");
-if (CTAELEMENT.hasAttribute("target")) {
-    console.log(CTAELEMENT.getAttribute("target"));
-} else {
-    CTAELEMENT.setAttribute("target", "_blank");
-}
-console.log(CTAELEMENT.attributes);
+const FEATURED = document.querySelector(".featured-image");
+const THEIMAGE = FEATURED.querySelector("img");
+
+var altText = THEIMAGE.getAttribute("alt");
+var captionElement = document.createElement("figcaption");
+var captionText = document.createTextNode(altText);
+captionElement.appendChild(captionText);
+
+console.log(captionElement);
+FEATURED.appendChild(captionElement);
+THEIMAGE.setAttribute("alt", "")
