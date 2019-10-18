6ba1b3f8e5be61cd41a6723e7ee049b738d72883
Ihar Sakun <watsonik@gmail.com>
2019-10-18 12:15:31 +0300

    Events - Pass arguments via event listeners

--- JS/script.js
+++ JS/script.js
@@ -4,13 +4,15 @@ const ALERT = document.querySelector("#booking-alert");
 CTA.classList.remove("hide");
 ALERT.classList.add("hide");
 
-function reveal(e) {
+function reveal(e, current) {
     e.preventDefault();
-    CTA.classList.toggle("hide");
+    current.innerHTML == "Book now!" ? CTA.innerHTML = "Oooops!" : CNT.innerHTML = "Book now!";
     ALERT.classList.toggle("hide");
 }
 
-CTA.addEventListener("click", reveal, false);
+CTA.addEventListener("click", function (e) {
+    reveal(e, this);
+}, false);
 CTA.addEventListener("click", function () {
     console.log("The button was clicked");
 }, false);
